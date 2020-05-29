f0277129f45067754d74bc868274c1c03fcfa025
Farsana <farzsadath8@gmail.com>
2020-05-29 12:57:52 +0530

    Spring Boot Init

---
+++ src/test/java/ndk/banee/spring_estore/SpringEStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ndk.banee.spring_estore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
